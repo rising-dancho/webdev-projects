ac07c537a8820fc79c30c2de8459f81922d6fe7d
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2023-08-25 08:58:45 +0800

    asdasd

--- traversy_media/copy to clipboard/script.js
+++ traversy_media/copy to clipboard/script.js
@@ -13,15 +13,21 @@ document.querySelectorAll(".copy-text").forEach((copyLinkParent) => {
 
         inputField.select();
         navigator.clipboard.writeText(text);
+        // change the button to green to indicate success
         copyButton.style.backgroundColor = '#27C451';
         copyButton.innerHTML = '✓';
+        // remove the blue outline on the input field upon button click
+        inputField.blur();
        
         // add the hidden pop-up component
         copyText.classList.add("active");
+
         setTimeout(() => {
+            // remove the hidden pop-up component
             copyText.classList.remove("active");
+            // change back button changes to original
             copyButton.style.backgroundColor = '#4D90FD';
             copyButton.innerHTML = 'Copy';
-        }, 1800);
+        }, 2000);
     });
 });
