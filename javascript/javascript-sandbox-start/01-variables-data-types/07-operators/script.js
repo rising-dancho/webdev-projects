2976bff287d30fac866885c0cb259580792a1429
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2023-09-04 19:15:53 +0800

    asdads

--- javascript/javascript-sandbox-start/01-variables-data-types/07-operators/script.js
+++ javascript/javascript-sandbox-start/01-variables-data-types/07-operators/script.js
@@ -1,7 +1,27 @@
 // 1. Arithmetic Operators
+let x;
+
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 5 % 5;
+
+// Concatenation
+x = 'Hello' + ' ' + 'World';
+
+// Exponent
+x = 2 ** 3
+
+// Increment
+x = 1;
 
 
 // 2. Assignment Operators
 
 
 // 3. Comparison Operators
+
+
+
+console.log(x);
