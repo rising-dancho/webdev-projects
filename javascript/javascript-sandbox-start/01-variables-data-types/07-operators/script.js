a8f3a414bfaa72fa5100a27cbe035ddb9bdaa1de
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2023-09-04 19:27:30 +0800

    asdasda

--- javascript/javascript-sandbox-start/01-variables-data-types/07-operators/script.js
+++ javascript/javascript-sandbox-start/01-variables-data-types/07-operators/script.js
@@ -19,22 +19,31 @@ x = 1;
 x++;
 
 //  Decrement
-x = x -1;
+x = x - 1;
 x--;
 
 // 2. Assignment Operators
 
 x = 10;
 
-x+=5;
-x-=5;
-x*=5;
-x/=5;
-x%=5;
-x**=5;
+x += 5;
+x -= 5;
+x *= 5;
+x /= 5;
+x %= 5;
+x **= 5;
 
 // 3. Comparison Operators
 
+x = 2 == '2'; // true
+x = 2 === '2'; // false
 
+x = 2 != '2'; // false
+x = 2 !== '2'; // true
+
+x = 10 > 5;
+x = 10 < 5;
+x = 10 >= 5;
+x = 10 <= 5;
 
 console.log(x);
