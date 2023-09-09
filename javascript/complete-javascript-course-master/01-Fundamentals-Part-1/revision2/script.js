e3f146a4317a3dd1b37e73380a664e17349a6cb5
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2023-09-09 17:39:18 +0800

    asdasd

--- javascript/complete-javascript-course-master/01-Fundamentals-Part-1/revision2/script.js
+++ javascript/complete-javascript-course-master/01-Fundamentals-Part-1/revision2/script.js
@@ -1,4 +1,31 @@
-let js = 'amazing';
-if(js==='amazing') alert('javascript is FUN!');
+'use strict';
 
-console.log(50+50);
+// let hasDriversLicense = false;
+// const passTest = true; 
+
+// if (passTest) hasDriversLicense = true;
+// if(hasDriversLicense) console.log(`i can drive 👍`);
+
+// // console.log(`drivers license? ${hasDriversLicense}`);
+
+// function
+// function logger(){
+//     console.log('my name is adfinem');
+// }
+
+// calling / running / invoking the function
+// logger();
+
+function fruitProcessor(apples, oranges){
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+};
+
+const appleJuice = fruitProcessor(5,1);
+console.log(`ordered apple beverage: ${appleJuice}`);
+
+const orangeJuice = fruitProcessor(1,4);
+console.log(`ordered orange beverage: ${orangeJuice}`);
+
+
+
