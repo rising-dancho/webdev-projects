f15099203be006395b4c3e95bac3e60347ef7e57
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2023-09-10 17:58:18 +0800

    sdfdsfdsf

--- javascript/complete-javascript-course-master/01-Fundamentals-Part-1/revision2/script.js
+++ javascript/complete-javascript-course-master/01-Fundamentals-Part-1/revision2/script.js
@@ -2,8 +2,12 @@
 
 // Your tasks:
 
-// Create an arrow function calcAverage to calculate the average of 3 scores. 
+// 1.) Create an arrow function calcAverage to calculate the average of 3 scores. 
 // This function should have three parameters and return a single number (the average score).
+
+// 2.) Create two new variables — scoreDolphins and scoreKoalas, 
+// and assign the value returned from the calcAverage 
+// function to them (you will need to call this function, and pass scores as arguments).
 
 const calcAverage = (team,score1, score2, score3) =>{
     const average = score1 + score2 + score3 / 3;
