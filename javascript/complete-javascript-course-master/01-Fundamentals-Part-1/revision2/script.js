92b6efdf08d2e332943be646066939a85fc9be05
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2023-09-10 18:42:49 +0800

    asdasd

--- javascript/complete-javascript-course-master/01-Fundamentals-Part-1/revision2/script.js
+++ javascript/complete-javascript-course-master/01-Fundamentals-Part-1/revision2/script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+
+// A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
+
 // Your tasks:
 
 // 1.) Create an arrow function calcAverage to calculate the average of 3 scores. 
@@ -9,10 +13,34 @@
 // and assign the value returned from the calcAverage 
 // function to them (you will need to call this function, and pass scores as arguments).
 
-const calcAverage = (team,score1, score2, score3) =>{
+// 3.) Create a function checkWinner that takes the average score of each team 
+// as parameters (avgDolphins and avgKoalas), and then logs the winner to the console, 
+// together with the victory points, according to the rule above. 
+// Example: Koalas win (30 vs. 13) (use avgDolphins and avgKoalas instead of hard-coded values).
+
+// TEST DATA 1: Dolphins scored 44, 23, and 71. Koalas scored 65, 54, and 49.
+
+// TEST DATA 2: Dolphins scored 85, 54, and 41. Koalas scored 23, 34, and 27.
+
+let scoreDolphins;
+let scoreKoalas;
+
+const calcAverage = (score1, score2, score3) => {
     const average = score1 + score2 + score3 / 3;
-    console.log(`${team} scored an average of ${average} points.`);
     return average;
 }
 
-console.log(calcAverage('adfinem', 90, 80,99));
+const avgDolphins = calcAverage(44, 23, 71);
+const avgKoalas = calcAverage(65, 54, 49);
+
+const checkWinner = (avgDolphins, avgKoalas) => {
+    if (avgDolphins > avgKoalas) {
+        console.log(`Dolphins won`);
+    } else {
+        console.log(`Koalas won`);
+    }
+}
+
+console.log(scoreDolphins);
+console.log(scoreKoalas);
+console.log(checkWinner(scoreDolphins, scoreKoalas));
