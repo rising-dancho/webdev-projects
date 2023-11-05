e1bd0890a6a39994015233ae1510d6f57417e66f
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2023-11-05 18:02:33 +0800

    zxczxc

---
+++ colt steele/PorfolioWebsite/index.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector("body")
+// const btn = document.querySelector("#btn")
+
+btn.addEventListener("click", () => {
+    // alert("you touch my tralala!!")
+    btn.classList.toggle("purple")
+});
+
