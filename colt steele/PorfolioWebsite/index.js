75fd75c434552280dc4de45e4adc55d5b6e4e8df
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2023-11-05 18:54:34 +0800

    zxczxc

---
+++ colt steele/PorfolioWebsite/index.js
@@ -0,0 +1,19 @@
+// const btn = document.querySelector("body")
+// // const btn = document.querySelector("#btn")
+
+// btn.addEventListener("click", () => {
+//     // alert("you touch my tralala!!")
+//     btn.classList.toggle("purple")
+// });
+
+const menu = document.querySelector(".hamburger");
+const bar1 = document.querySelector(".bar1");
+const bar2 = document.querySelector(".bar2");
+const bar3 = document.querySelector(".bar3");
+
+menu.addEventListener("click", () => {
+    // alert("clicked!")
+    bar1.classList.toggle("animateBar1");
+    bar2.classList.toggle("animateBar2");
+    bar3.classList.toggle("animateBar3");
+})
