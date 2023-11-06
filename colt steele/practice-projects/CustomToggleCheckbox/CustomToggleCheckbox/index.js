979b1f8b437ce5c693d08b4c0e9eabcad567b4e5
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2023-11-06 18:45:22 +0800

    zxczxc

--- colt steele/practice-projects/CustomToggleCheckbox/CustomToggleCheckbox/index.js
+++ colt steele/practice-projects/CustomToggleCheckbox/CustomToggleCheckbox/index.js
@@ -1,11 +1,24 @@
-const box = document.querySelector("#enabled");
-const box2 = document.querySelector("#dogs");
+// const box = document.querySelector("#enabled");
+// const box2 = document.querySelector("#dogs");
 
-box.addEventListener("click", function() {
-  alert(box.checked);
-});
+// box.addEventListener("click", function() {
+//   alert(box.checked);
+// });
 
-box2.addEventListener("click", function() {
-  alert(box2.checked);
-});
+// box2.addEventListener("click", function() {
+//   alert(box2.checked);
+// });
 
+function addToggleListener(selector) {
+    const box = document.querySelector(selector);
+  
+    if (box) {
+      box.addEventListener("click", function() {
+        alert(`Toggle with id ${selector} is checked: ${box.checked}`);
+      });
+    }
+  }
+  
+  addToggleListener("#enabled");
+  addToggleListener("#dogs");
+  
