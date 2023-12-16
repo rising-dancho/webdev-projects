8eb52e9b3e1a719ca144d8f0b1b66311b0518f1e
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2023-12-17 01:58:04 +0800

    zxc

--- jonas.io/Omnifood/js/script.js
+++ jonas.io/Omnifood/js/script.js
@@ -23,9 +23,9 @@ const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
+
 ///////////////////////////////////////////////////////////
-
-
+// // Make mobile navigation work
 const headerEl = document.querySelector(".section-header");
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 
@@ -33,29 +33,6 @@ btnNavEl.addEventListener("click", function() {
   headerEl.classList.toggle("nav-open");
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Make mobile navigation work
-
-// const btnNavEl = document.querySelector(".btn-mobile-nav");
-// const headerEl = document.querySelector(".header");
-
-// btnNavEl.addEventListener("click", function () {
-//   headerEl.classList.toggle("nav-open");
-// });
 
 ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
