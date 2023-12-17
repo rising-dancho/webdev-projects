6c5be2563a3fdddefa1e159ebffafe12b8897f57
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2023-12-18 04:53:52 +0800

    zxc

--- jonas.io/Omnifood/js/script.js
+++ jonas.io/Omnifood/js/script.js
@@ -31,7 +31,7 @@ const btnNavEl = document.querySelector(".btn-mobile-nav");
 
 btnNavEl.addEventListener("click", function() {
   headerEl.classList.toggle("nav-open");
-})
+});
 
 
 ///////////////////////////////////////////////////////////
@@ -63,35 +63,32 @@ links.forEach(function(link){
       headerEl.classList.toggle("nav-open");
     }
 
-  })
-})
+  });
+});
+
 
 ///////////////////////////////////////////////////////////
 // Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
 
-// const sectionHeroEl = document.querySelector(".section-hero");
+const observer = new IntersectionObserver(function(entries) {
+  const ent =  entries[0];
+  console.log(ent);
 
-// const obs = new IntersectionObserver(
-//   function (entries) {
-//     const ent = entries[0];
-//     console.log(ent);
-
-//     if (ent.isIntersecting === false) {
-//       document.body.classList.add("sticky");
-//     }
-
-//     if (ent.isIntersecting === true) {
-//       document.body.classList.remove("sticky");
-//     }
-//   },
-//   {
-//     // In the viewport
-//     root: null,
-//     threshold: 0,
-//     rootMargin: "-80px",
-//   }
-// );
-// obs.observe(sectionHeroEl);
+  if(ent.isIntersecting === false) {
+    document.body.classList.add("sticky-nav");
+  } else {
+    document.body.classList.remove("sticky-nav");
+  }
+},
+{
+  // root: null means it is observing inside of the viewport
+  root:null,
+  // fire as soon as zero percent of the viewport (root:null); null means viewport
+  threshold:0,
+  rootMargin:"-80px"
+});
+observer.observe(sectionHeroEl);
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
