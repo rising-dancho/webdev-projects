a1532f109a42d414560e9251264caf1ed47adb93
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2023-12-17 14:31:48 +0800

    zxc

--- jonas.io/Omnifood/js/script.js
+++ jonas.io/Omnifood/js/script.js
@@ -36,32 +36,35 @@ btnNavEl.addEventListener("click", function() {
 
 ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
+const links = document.querySelectorAll("a:link");
+// console.log(links);
 
-const allLinks = document.querySelectorAll("a:link");
-
-allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
+links.forEach(function(link){
+  link.addEventListener("click", function(e){
+    // e.preventDefault();
     const href = link.getAttribute("href");
 
-    // Scroll back to top
-    if (href === "#")
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
+    // scrollback to top
+    if (href === "#") window.scrollTo({
+      top:-10,
+      behavior:"smooth",
+    })
 
-    // Scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
+    // scroll to clicked link
+    // if (href !== "#" && href.startsWith("#")) {
+    //   const sectionEl = document.querySelector(href);
+    //   sectionEl.scrollIntoView({
+    //     behavior:"smooth"
+    //   })
+    // }
+
+    // close mobile navigation
+    if(link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
     }
 
-    // Close mobile naviagtion
-    if (link.classList.contains("main-nav-link"))
-      headerEl.classList.toggle("nav-open");
-  });
-});
+  })
+})
 
 ///////////////////////////////////////////////////////////
 // Sticky navigation
