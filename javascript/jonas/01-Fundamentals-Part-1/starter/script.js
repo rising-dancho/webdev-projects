c8fa0d4ce965ec8f4d9a86db6e0fdb3cc84a98bd
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2023-12-26 06:56:59 +0800

    zxc

--- javascript/jonas/01-Fundamentals-Part-1/starter/script.js
+++ javascript/jonas/01-Fundamentals-Part-1/starter/script.js
@@ -280,26 +280,26 @@ console.log(Number(inputYear) + 18);
 
 
 
-// BMI CALC
-//Create your function below this line.
-function bmiCalculator(weight, height){
-    const resultBMI = Math.round(weight / (height*height));
-    return resultBMI;
-}
+// // BMI CALC
+// //Create your function below this line.
+// function bmiCalculator(weight, height){
+//     const resultBMI = Math.round(weight / Math.pow(height,2));
+//     return resultBMI;
+// }
 
-//The first parameter should be the weight and the second should be the height.
+// //The first parameter should be the weight and the second should be the height.
 
-const weight = prompt(`What is your current weight(kg)?`);
-const height = prompt(`What is your height(m)?`);
+// const weight = prompt(`What is your current weight(kg)?`);
+// const height = prompt(`What is your height(m)?`);
 
-alert(bmiCalculator(weight, height));
+// alert(bmiCalculator(weight, height));
 
 
-/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+// /* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
 
-var bmi = bmiCalculator(65, 1.8); 
+// var bmi = bmiCalculator(65, 1.8); 
 
-bmi should equal 20 when it's rounded to the nearest whole number.
+// bmi should equal 20 when it's rounded to the nearest whole number.
 
-*/
+// */
 
