4cd8642935a097b50ea99d54819e73dfc4123f12
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2023-12-31 11:51:48 +0800

    zxc

--- javascript/jonas/01-Fundamentals-Part-1/starter/DOM Challenge Starting Files/index.js
+++ javascript/jonas/01-Fundamentals-Part-1/starter/DOM Challenge Starting Files/index.js
@@ -1,6 +1,10 @@
 // document.getElementsByTagName("li")[1].style.color = "red";
 
-document.querySelector(".list a").style.color = "red";
+// document.querySelector(".list a").style.color = "red";
 
-document.querySelector("button").style.backgroundColor = "yellow"
-document.querySelector("button").style.cursor = "pointer"
+// document.querySelector("button").style.backgroundColor = "yellow"
+// document.querySelector("button").style.cursor = "pointer"
+
+
+document.querySelector("button").classList.toggle("invisible");
+document.querySelector("h1").classList.toggle("huge");
