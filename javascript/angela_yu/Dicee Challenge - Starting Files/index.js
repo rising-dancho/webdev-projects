f4223dccf6596761fb02425dec2964ca4cfba6f9
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-03 10:47:19 +0800

    zxc

--- javascript/angela_yu/Dicee Challenge - Starting Files/index.js
+++ javascript/angela_yu/Dicee Challenge - Starting Files/index.js
@@ -4,11 +4,19 @@ let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 // console.log(randomNumber1);
 
 document.querySelector("img").getAttribute("src");
+
+// document.querySelectorAll("img")[0].setAttribute("src",`./images/dice${randomNumber1}.png`);
+// document.querySelectorAll("img")[1].setAttribute("src",`./images/dice${randomNumber2}.png`);
+
 document.querySelector("img.img1").setAttribute("src", `./images/dice${randomNumber1}.png`);
 document.querySelector("img.img2").setAttribute("src", `./images/dice${randomNumber2}.png`);
 
-if(randomNumber1 > randomNumber2) {
+if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
-}else {
+} else if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw!";
+} else {
     document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
 }
+
+
