5ccec3393346251ba6265d26aa843179b67a1930
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-09 08:31:35 +0800

    zxc

--- javascript/jonas/script.js
+++ javascript/jonas/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // // BMI = mass / (height * height)
 
 
@@ -188,11 +190,11 @@
 
 // TEST DATA: Test with different bill values: 275, 40, and 430
 
-function tipCalculator(bill) {
-    let tip;
-    return tip = bill >= 50 && bill <= 300 ? bill * .15 : bill * .20;
-}
+// function tipCalculator(bill) {
+//     let tip;
+//     return tip = bill >= 50 && bill <= 300 ? bill * .15 : bill * .20;
+// }
 
-const bill = [275, 40, 430];
+// const bill = [275, 40, 430];
 
-console.log(`The bill was ${bill[0]}, the tip was ${tipCalculator(bill[0])}, and the total value ${bill[0] + tipCalculator(bill[0])}`);
+// console.log(`The bill was ${bill[0]}, the tip was ${tipCalculator(bill[0])}, and the total value ${bill[0] + tipCalculator(bill[0])}`);
