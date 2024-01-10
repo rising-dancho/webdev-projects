125d636984df766ca65da3ea147b6d07953b3e07
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-10 09:35:09 +0800

    zxc

--- javascript/jonas/script.js
+++ javascript/jonas/script.js
@@ -1,4 +1,13 @@
-const friends = ['Ben', 'Ten', 'Nabuto'];
-friends.push('Vitlog');
+// const friends = ['Ben', 'Ten', 'Nabuto'];
+// friends.push('Vitlog');
 
-console.log(friends);
+// console.log(friends);
+
+function tipCalculator(bill) {
+    let tip;
+    return tip = bill >= 50 && bill <= 300 ? bill * .15 : bill * .20;
+}
+
+const bill = [275, 40, 430];
+
+console.log(`The bill was ${bill[0]}, the tip was ${tipCalculator(bill[0])}, and the total value ${bill[0] + tipCalculator(bill[0])}`);
