765cedfec12441fe9662714c8df21a18b75bab81
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-11 08:31:44 +0800

    zxc

--- javascript/jonas/complete/script.js
+++ javascript/jonas/complete/script.js
@@ -789,33 +789,33 @@ TEST DATA: Test with different bill values: 275, 40, and 430
 */
 // ternary operator syntax : condition ? action : else (this part is mandatory).
 
-let bill = 275;
+// let bill = 275;
 
-let choice = prompt(`
-Bill Amount: 
-a.) 275 dollars
-b.) 40 dollars
-c.) 430 dollars`
-);
+// let choice = prompt(`
+// Bill Amount: 
+// a.) 275 dollars
+// b.) 40 dollars
+// c.) 430 dollars`
+// );
 
 
-switch (choice) {
-    case 'a':
-        bill = 275;
-        break;
+// switch (choice) {
+//     case 'a':
+//         bill = 275;
+//         break;
     
-    case 'b':
-        bill = 40;
-        break;
+//     case 'b':
+//         bill = 40;
+//         break;
     
-    case 'c':
-        bill = 430;
-        break;
-    default:
-        console.log(`pls write a, b, or c. no other choices are allowed.`)
-}
+//     case 'c':
+//         bill = 430;
+//         break;
+//     default:
+//         console.log(`pls write a, b, or c. no other choices are allowed.`)
+// }
 
-const tip = bill >= 50 && bill <= 300 ? bill *.15 : bill*.20;
+// const tip = bill >= 50 && bill <= 300 ? bill *.15 : bill*.20;
 
 // let tipPercentage;
 // determine tip percentage
@@ -826,7 +826,7 @@ const tip = bill >= 50 && bill <= 300 ? bill *.15 : bill*.20;
 // }
 
 
-console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
 // console.log(`-  you spent ${bill} dollars. so the tip should be ${tip} dollars. which is ${tipPercentage} percent of your spending.`);
 
 
@@ -1085,27 +1085,60 @@ console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bil
 
 // A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
 
-const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+// const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
-// A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
-function checkWinner(avgDolphins, avgKoalas) {
-    const doubleAvgDolphins = avgDolphins * 2;
-    const doubleAvgKoalas = avgKoalas * 2;
-    if (avgDolphins >= doubleAvgKoalas) {
-        console.log(`Dolphins win 🏆(${avgDolphins} vs. ${avgKoalas})`);
-    } else if (avgKoalas >= doubleAvgDolphins) {
-        console.log(`Koalas win 🏆(${avgKoalas} vs. ${avgDolphins})`);
-    } else {
-        console.log(`No team wins...`);
-    }
-}
-// TEST 1
-let scoreDolphins = calcAverage(44, 23, 71);
-let scoreKoalas = calcAverage(65, 54, 49);
-checkWinner(scoreDolphins, scoreKoalas);
+// // A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
+// function checkWinner(avgDolphins, avgKoalas) {
+//     const doubleAvgDolphins = avgDolphins * 2;
+//     const doubleAvgKoalas = avgKoalas * 2;
+//     if (avgDolphins >= doubleAvgKoalas) {
+//         console.log(`Dolphins win 🏆(${avgDolphins} vs. ${avgKoalas})`);
+//     } else if (avgKoalas >= doubleAvgDolphins) {
+//         console.log(`Koalas win 🏆(${avgKoalas} vs. ${avgDolphins})`);
+//     } else {
+//         console.log(`No team wins...`);
+//     }
+// }
+// // TEST 1
+// let scoreDolphins = calcAverage(44, 23, 71);
+// let scoreKoalas = calcAverage(65, 54, 49);
+// checkWinner(scoreDolphins, scoreKoalas);
 
-// TEST 2
-scoreDolphins = calcAverage(85, 54, 41);
-scoreKoalas = calcAverage(23, 34, 27);
-checkWinner(scoreDolphins, scoreKoalas);
+// // TEST 2
+// scoreDolphins = calcAverage(85, 54, 41);
+// scoreKoalas = calcAverage(23, 34, 27);
+// checkWinner(scoreDolphins, scoreKoalas);
 
+// const friends = ['Ben', 'Ten', 'Nabuto'];
+// friends.push('Vitlog');
+
+// console.log(friends);
+
+// CHALLENGE #2
+// Steven wants you to improve his tip calculator, using the same rules as before — tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+// Your tasks:
+
+// Write a function calcTip that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from the first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+
+// And now let's use arrays! So, create an array called bills containing the test data below.
+
+// Create an array called tips containing the tip value for each bill, calculated from the function you created before.
+
+// BONUS: Create an array totals containing the total values, so the bill + tip.
+
+// TEST DATA: 125, 555, and 44.
+
+
+
+// 👋 OPTIONAL: You can watch my solution in video format in the next lecture
+
+// function calcTip(bill) {
+//     return bill >= 50 && bill <= 300 ? bill * .15 : bill * .20;
+// }
+
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+
+// console.log(`The bill was ${bills[0]}, the tip was ${tips[0]}, and the total value ${bills[0] + tips[0]}`);
--- javascript/jonas/script.js
+++ javascript/jonas/script.js
@@ -1,37 +1,3 @@
-// const friends = ['Ben', 'Ten', 'Nabuto'];
-// friends.push('Vitlog');
-
-// console.log(friends);
-
-// CHALLENGE #2
-// Steven wants you to improve his tip calculator, using the same rules as before — tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
-
-// Your tasks:
-
-// Write a function calcTip that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from the first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
-
-// And now let's use arrays! So, create an array called bills containing the test data below.
-
-// Create an array called tips containing the tip value for each bill, calculated from the function you created before.
-
-// BONUS: Create an array totals containing the total values, so the bill + tip.
-
-// TEST DATA: 125, 555, and 44.
-
-
-
-// 👋 OPTIONAL: You can watch my solution in video format in the next lecture
-
-// function calcTip(bill) {
-//     return bill >= 50 && bill <= 300 ? bill * .15 : bill * .20;
-// }
-
-// const bills = [125, 555, 44];
-// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-
-
-// console.log(`The bill was ${bills[0]}, the tip was ${tips[0]}, and the total value ${bills[0] + tips[0]}`);
-
 const jonasArray = [
     'Jonas',
     'Schmedtmann',
@@ -63,3 +29,12 @@ ${jose['friends']}`);
 const nameKey = 'Name';
 console.log(`${jose['first' + nameKey]}`);
 console.log(`${jose['last' + nameKey]}`);
+
+const interestedIn = prompt(`What do you want to know about Jose?
+Choose between firstName, lastName, age, job, and friends`);
+
+if(jonas[interestedIn]){
+    console.log(jonas[interestedIn]);
+}else {
+    console.log(`Wrong entry! Please choose between firstName, lastName, age, job, and friends`);
+}
