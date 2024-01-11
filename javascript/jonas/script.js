81ae0e8dfc4714071d9a9bd1478e26cb8ce5f6fd
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-11 09:05:46 +0800

    zxc

--- javascript/jonas/script.js
+++ javascript/jonas/script.js
@@ -14,27 +14,34 @@ const jose = {
     friends: ['n/a']
 };
 
-console.log(`${jose.firstName}
-${jose.lastName}
-${jose.age}
-${jose.job}
-${jose.friends}`);
+// console.log(`${jose.firstName}
+// ${jose.lastName}
+// ${jose.age}
+// ${jose.job}
+// ${jose.friends}`);
 
-console.log(`${jose['firstName']}
-${jose['lastName']}
-${jose['age']}
-${jose['job']}
-${jose['friends']}`);
+// console.log(`${jose['firstName']}
+// ${jose['lastName']}
+// ${jose['age']}
+// ${jose['job']}
+// ${jose['friends']}`);
 
-const nameKey = 'Name';
-console.log(`${jose['first' + nameKey]}`);
-console.log(`${jose['last' + nameKey]}`);
+// const nameKey = 'Name';
+// console.log(`${jose['first' + nameKey]}`);
+// console.log(`${jose['last' + nameKey]}`);
 
-const interestedIn = prompt(`What do you want to know about Jose?
-Choose between firstName, lastName, age, job, and friends`);
+// const interestedIn = prompt(`What do you want to know about Jose?
+// Choose between firstName, lastName, age, job, and friends`);
 
-if(jonas[interestedIn]){
-    console.log(jonas[interestedIn]);
-}else {
-    console.log(`Wrong entry! Please choose between firstName, lastName, age, job, and friends`);
-}
+// if(jonas[interestedIn]){
+//     console.log(jonas[interestedIn]);
+// }else {
+//     console.log(`Wrong entry! Please choose between firstName, lastName, age, job, and friends`);
+// }
+
+jose.location = 'Philippines';
+jose.sport = 'Mauy Thai';
+
+
+console.log(jose.location);
+console.log(jose.sport);
