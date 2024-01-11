ab0ce3e4496a57b73f5b1a8aa23007af2128a445
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-11 19:26:38 +0800

    zxc

--- javascript/jonas/script.js
+++ javascript/jonas/script.js
@@ -25,28 +25,36 @@
 // BMI = mass / (height * height
 // Mark Miller and John Smith
 
-function calcBMI(mass, height){
-    const bmi =  mass / (height * height);
-    return bmi;
-}
-
 const mark = {
-    fullName: `Mark Miller`,
-    mass: 78, //kg
-    height: 1.69, //meters
-    bmi: calcBMI(mark.mass, mark.height)
-};
-
-const john = {
-    fullName: `John Smith`,
-    mass: 92, 
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function () {
+      this.bmi = this.mass / this.height * this.height;
+      return this.bmi;
+    }
+  };
+  
+  const john = {
+    fullName: 'John Smith',
+    mass: 92,
     height: 1.95,
-    bmi: calcBMI(john.mass, john.height)
-};
-
-
-if(mark.bmi > john.bmi) {
-    console.log(`John Smith's BMI (${john.bmi}) is higher than Mark Miller's (${mark.bmi})!`);
-}else {
-     console.log(`Mark Miller's BMI (${mark.bmi}) is higher than John Smith's (${john.bmi})!`);
-}
+    calcBMI: function () {
+      this.bmi = this.mass / this.height * this.height;
+      return this.bmi;
+    }
+  };
+  
+  mark.calcBMI();
+  john.calcBMI();
+  
+  console.log(mark.bmi, john.bmi);
+  
+  // "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+  
+  if (mark.bmi > john.bmi) {
+    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`)
+  } else if (john.bmi > mark.bmi) {
+    console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`)
+  }
+  
