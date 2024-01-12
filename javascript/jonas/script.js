521e67a05838a7ef37c885388676da0cee2f6197
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-13 07:53:47 +0800

    zxc

--- javascript/jonas/script.js
+++ javascript/jonas/script.js
@@ -1,10 +1,24 @@
-const years = [1991, 2007, 1969, 1985];
-const ages = [];
+// const years = [1991, 2007, 1969, 1985];
+// const ages = [];
 
-for (let i = 0; i < years.length; i++) {
-  // ages = 2024 - 1991
-  let age = 2024 - years[i];
-  ages.push(age);
-  console.log(ages);
+// for (let i = 0; i < years.length; i++) {
+//   // ages = 2024 - 1991
+//   let age = 2024 - years[i];
+//   ages.push(age);
+//   console.log(ages);
+// }
+
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1992,
+  'teacher',
+  ['Micheal', 'Peter', 'Steven']
+];
+
+// continue
+console.log('----ONLY STRINGS----');
+for (let i=0; i<=jonas.length;i++){
+  if(typeof jonas[i] !== 'string') continue;
+  console.log(jonas[i], typeof jonas[i]);
 }
-
