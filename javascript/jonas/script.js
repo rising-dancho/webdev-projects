8c79f655e4ffba4d75a903272cc0126ed11de58e
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-12 10:14:12 +0800

    zxc

--- javascript/jonas/script.js
+++ javascript/jonas/script.js
@@ -1,5 +1,18 @@
 // while condition is TRUE
 // `Lifting weigt repetition 1`
-for(let rep = 1; rep <=10; rep++){
-  console.log(`Lifting weigt repetition ${rep}`);
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`Lifting weigt repetition ${rep}`);
+// }
+
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1992,
+  'teacher',
+  ['Micheal', 'Peter', 'Steven']
+];
+
+
+for (let i = 0; i < 5; i++) {
+  console.log(jonas[i]);
 }
