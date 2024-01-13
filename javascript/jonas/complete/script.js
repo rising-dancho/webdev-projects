9b901568f528271b089444a5e7097fcae948c3d1
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-13 15:53:44 +0800

    zxc

--- javascript/jonas/complete/script.js
+++ javascript/jonas/complete/script.js
@@ -1340,3 +1340,27 @@ TEST DATA: Test with different bill values: 275, 40, and 430
 //   if (typeof jonas[i] === 'number') break;
 //   console.log(jonas[i], typeof jonas[i]);
 // }
+
+
+// const jonas = [
+//     'Jonas',
+//     'Schmedtmann',
+//     2037 - 1992,
+//     'teacher',
+//     ['Micheal', 'Peter', 'Steven']
+//   ];
+  
+//   // continue : exit (skip) the current iteration of the loop (skip current count)
+//   console.log('----ONLY STRINGS----');
+//   for (let i=0; i<=jonas.length;i++){
+//     if(typeof jonas[i] !== 'string') continue;
+//     console.log(jonas[i], typeof jonas[i]);
+//   }
+  
+//   // break : terminates the loop or switch statement
+//   console.log('----BREAK UPON SEEING A NUMBER----');
+//   for (let i=0; i<=jonas.length;i++){
+//     if(typeof jonas[i] === 'number') break;
+//     console.log(jonas[i], typeof jonas[i]);
+//   }
+  
--- javascript/jonas/script.js
+++ javascript/jonas/script.js
@@ -6,16 +6,6 @@ const jonas = [
   ['Micheal', 'Peter', 'Steven']
 ];
 
-// continue : exit (skip) the current iteration of the loop (skip current count)
-console.log('----ONLY STRINGS----');
-for (let i=0; i<=jonas.length;i++){
-  if(typeof jonas[i] !== 'string') continue;
-  console.log(jonas[i], typeof jonas[i]);
-}
-
-// break : terminates the loop or switch statement
-console.log('----BREAK UPON SEEING A NUMBER----');
-for (let i=0; i<=jonas.length;i++){
-  if(typeof jonas[i] === 'number') break;
-  console.log(jonas[i], typeof jonas[i]);
+for(let i = jonas.length -1; i >= 0; i--){
+console.log(jonas[i]);
 }
