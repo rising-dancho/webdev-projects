88ef2d8cb7b1e4a8fa968599df7183e3a47082de
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-15 08:43:09 +0800

    zxc

--- javascript/jonas/script.js
+++ javascript/jonas/script.js
@@ -18,4 +18,30 @@ for (let i = 0; i < bills.length; i++) {
     totals.push(total);
 }
 
-console.log(`bills: ${bills}`, `tips: ${tips}`, `total: ${totals}`);
+console.log(
+    ` bills: ${bills}
+`, `tips: ${tips}
+`, `total: ${totals}`);
+
+
+function calcAverage(arr) {
+    // This function calculates the average of all numbers in the given array.
+    let sum = 0;
+    for (let i=0; i<totals.length;i++) {
+        sum += totals[i];
+    }
+    console.log(` sum total: ${sum}`)
+    return sum / totals.length;
+}
+
+console.log(` average: ${calcAverage(totals)}`);
+
+// First, you will need to add up all values in the array. To do the addition, start by creating a variable sum that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the sum variable. This way, by the end of the loop, you have all values added together.
+
+// To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements).
+
+// Call the function with the totals array.
+
+
+
+
