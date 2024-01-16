08c9f4bbd09029a9deba1445883160b2f09ebf36
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-17 05:27:43 +0800

    zxc

--- javascript/jonas/script.js
+++ javascript/jonas/script.js
@@ -5,12 +5,10 @@
 ///////////////////////////////////////
 // Using Google, StackOverflow and MDN
 
-// PROBLEM: 1:
+// PROBLEM:
 // We work for a company building a smart home thermometer. 
 // Our most recent task is this: "Given an array of temperatures of one day, 
 // calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
-
-const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
 // 1) Understanding the problem
 // What is amplitude? 
@@ -26,19 +24,38 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 // - Find min value in temp array
 // - Subtract min from max (amplitude) and return it
 
-const calcTempAmplitude = function (temps) {
-  let max = temps[0];
-  let min = temps[0];
 
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== 'number') continue;
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
-  }
-  console.log(max, min);
-  return max - min;
-};
-const amplitude = calcTempAmplitude(temperatures);
-console.log(amplitude);
+function calcAmplitude(temp) {
+
+  let max = temp[0];
+  let min = temp[0];
+
+  for (let i = 0; i < temp.length; i++) {
+    let currentTemp = temp[i];
+    
+    if(typeof currentTemp !== 'number') continue;
+    if (currentTemp > max) {
+      max = currentTemp;
+    } else if (currentTemp < min) {
+      min = currentTemp
+    }
+  }       
+  const amplitude = max - min; 
+  return `max: ${max}; min: ${min}; amplitude: ${amplitude}`;
+}
+
+console.log(calcAmplitude(temperatures));
+
+
+// // For regular arrays:
+// var max = Math.max(arrayOfNumbers);
+
+// // For arrays with tens of thousands of items:
+// let max = testArray[0];
+// for (let i = 1; i < testArrayLength; ++i) {
+//   if (testArray[i] > max) {
+//     max = testArray[i];
+//   }
+// }
