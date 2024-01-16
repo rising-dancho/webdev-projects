a207f1490c2c2673dbb5766255eec1c83f138e68
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-16 10:27:15 +0800

    zxc

--- javascript/jonas/complete/script.js
+++ javascript/jonas/complete/script.js
@@ -1392,49 +1392,49 @@ TEST DATA: Test with different bill values: 275, 40, and 430
 //   if(dice !== 6) console.log('Loop is about to end...'); 
 // }
 
-const calcTip = function (bill) {
-    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-}
+// const calcTip = function (bill) {
+//     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// }
 
-/* Write your code below. Good luck! 🙂 */
+// /* Write your code below. Good luck! 🙂 */
 
-let bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// let bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 
-const tips = [];
-const totals = [];
-
-
-for (let i = 0; i < bills.length; i++) {
-    // console.log(calcTip(bills[i]));
-    tips.push(calcTip(bills[i]));
-    // console.log(tips);
-    let total = tips[i] + bills[i];
-    totals.push(total);
-}
-
-console.log(
-    ` bills: ${bills}
-`, `tips: ${tips}
-`, `total: ${totals}`);
+// const tips = [];
+// const totals = [];
 
 
-function calcAverage(arr) {
-    // This function calculates the average of all numbers in the given array.
-    let sum = 0;
-    for (let i=0; i<totals.length;i++) {
-        sum += totals[i];
-    }
-    console.log(` sum total: ${sum}`)
-    return sum / totals.length;
-}
+// for (let i = 0; i < bills.length; i++) {
+//     // console.log(calcTip(bills[i]));
+//     tips.push(calcTip(bills[i]));
+//     // console.log(tips);
+//     let total = tips[i] + bills[i];
+//     totals.push(total);
+// }
 
-console.log(` average: ${calcAverage(totals)}`);
+// console.log(
+//     ` bills: ${bills}
+// `, `tips: ${tips}
+// `, `total: ${totals}`);
 
-// First, you will need to add up all values in the array. To do the addition, start by creating a variable sum that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the sum variable. This way, by the end of the loop, you have all values added together.
 
-// To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements).
+// function calcAverage(arr) {
+//     // This function calculates the average of all numbers in the given array.
+//     let sum = 0;
+//     for (let i=0; i<totals.length;i++) {
+//         sum += totals[i];
+//     }
+//     console.log(` sum total: ${sum}`)
+//     return sum / totals.length;
+// }
 
-// Call the function with the totals array.
+// console.log(` average: ${calcAverage(totals)}`);
+
+// // First, you will need to add up all values in the array. To do the addition, start by creating a variable sum that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the sum variable. This way, by the end of the loop, you have all values added together.
+
+// // To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements).
+
+// // Call the function with the totals array.
 
 
 
--- javascript/jonas/script.js
+++ javascript/jonas/script.js
@@ -1,49 +1,161 @@
-const calcTip = function (bill) {
-    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-}
-
-/* Write your code below. Good luck! 🙂 */
-
-let bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-
-const tips = [];
-const totals = [];
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
 
 
-for (let i = 0; i < bills.length; i++) {
-    // console.log(calcTip(bills[i]));
-    tips.push(calcTip(bills[i]));
-    // console.log(tips);
-    let total = tips[i] + bills[i];
-    totals.push(total);
-}
+///////////////////////////////////////
+// Using Google, StackOverflow and MDN
 
-console.log(
-    ` bills: ${bills}
-`, `tips: ${tips}
-`, `total: ${totals}`);
+// PROBLEM 1:
+// We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// 1) Understanding the problem
+// - What is temp amplitude? Answer: difference between highest and lowest temp
+// - How to compute max and min temperatures?
+// - What's a sensor error? And what do do?
+
+// 2) Breaking up into sub-problems
+// - How to ignore errors?
+// - Find max value in temp array
+// - Find min value in temp array
+// - Subtract min from max (amplitude) and return it
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
+
+// PROBLEM 2:
+// Function should now receive 2 arrays of temps
+
+// 1) Understanding the problem
+// - With 2 arrays, should we implement functionality twice? NO! Just merge two arrays
+
+// 2) Breaking up into sub-problems
+// - Merge 2 arrays
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
 
 
-function calcAverage(arr) {
-    // This function calculates the average of all numbers in the given array.
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i];
-    }
-    console.log(` sum total: ${sum}`)
-    return sum / arr.length;
-}
+///////////////////////////////////////
+// Debugging with the Console and Breakpoints
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
 
-console.log(` average: ${calcAverage(totals)}`);
+    // C) FIX
+    // value: Number(prompt('Degrees celsius:')),
+    value: 10,
+  };
 
-// First, you will need to add up all values in the array. To do the addition, start by creating a variable sum that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the sum variable. This way, by the end of the loop, you have all values added together.
+  // B) FIND
+  console.table(measurement);
 
-// To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements).
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
 
-// Call the function with the totals array.
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A) IDENTIFY
+console.log(measureKelvin());
+
+// Using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// A) IDENTIFY
+console.log(amplitudeBug);
 
 
+///////////////////////////////////////
+// Coding Challenge #1
 
-console.log();
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
 
-// PROBLEM: 
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+
+/*
+// 1) Understanding the problem
+// - Array transformed to string, separated by ...
+// - What is the X days? Answer: index + 1
+
+// 2) Breaking up into sub-problems
+// - Transform array into string
+// - Transform each element to string with ºC
+// - Strings needs to contain day (index + 1)
+// - Add ... between elements and start and end of string
+// - Log string to console
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+console.log(`... ${data1[0]}ºC ... ${data1[1]}ºC ... ${data1[2]}ºC ...`);
+
+const printForecast = function (arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}ºC in ${i + 1} days ... `;
+  }
+  console.log('...' + str);
+};
+printForecast(data1);
+*/
+
