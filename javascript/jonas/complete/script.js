a6752e2fd516bd6ac8ba4cf5ca5a90ba43e0f44b
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-18 03:27:24 +0800

    zxc

--- javascript/jonas/complete/script.js
+++ javascript/jonas/complete/script.js
@@ -1491,27 +1491,44 @@ TEST DATA: Test with different bill values: 275, 40, and 430
 // TODO: make the function receive two arrays of temparatures 
 
 
-function calcAmplitudeNew(temp1, temp2) {
+// function calcAmplitudeNew(temp1, temp2) {
 
-  let temperatures = temp1.concat(temp2);
+//   let temperatures = temp1.concat(temp2);
 
-  let max = temperatures[0];
-  let min = temperatures[0];
+//   let max = temperatures[0];
+//   let min = temperatures[0];
 
-  for (let i = 0; i < temperatures.length; i++) {
-    let currentTemp = temperatures[i];
+//   for (let i = 0; i < temperatures.length; i++) {
+//     let currentTemp = temperatures[i];
 
-    if (typeof currentTemp !== 'number') continue;
-    if (currentTemp > max) {
-      max = currentTemp;
-    } else if (currentTemp < min) {
-      min = currentTemp
-    }
-  }
-  const amplitude = max - min;
-  return `max: ${max}; min: ${min}; amplitude: ${amplitude}`;
-}
-const temp1 = [3,1,1];
-const temp2 = [6,2,2];
+//     if (typeof currentTemp !== 'number') continue;
+//     if (currentTemp > max) {
+//       max = currentTemp;
+//     } else if (currentTemp < min) {
+//       min = currentTemp
+//     }
+//   }
+//   const amplitude = max - min;
+//   return `max: ${max}; min: ${min}; amplitude: ${amplitude}`;
+// }
+// const temp1 = [3,1,1];
+// const temp2 = [6,2,2];
 
-calcAmplitudeNew(temp1, temp2);
+// calcAmplitudeNew(temp1, temp2);
+
+
+// function measureKelvin(){
+//   const measurement = {
+//     type: 'temp',
+//     unit: 'celsius',
+//     // 3. FIX THE BUG 
+//     value: Number(prompt('Degrees celsius:'))
+//   }
+//   // 2. FIND THE BUG 
+//   console.table(measurement);
+//   debugger;
+//   const kelvin = measurement.value + 273;
+//   return kelvin;
+// }
+// // 1. IDENTIFY 
+// console.log(measureKelvin());
--- javascript/jonas/script.js
+++ javascript/jonas/script.js
@@ -1,15 +1,24 @@
+function calcAmplitudeNew(temp1, temp2) {
 
-function measureKelvin(){
-  const measurement = {
-    type: 'temp',
-    unit: 'celsius',
-    // 3. FIX THE BUG 
-    value: Number(prompt('Degrees celsius:'))
+  let temperatures = temp1.concat(temp2);
+
+  let max = temperatures[0];
+  let min = temperatures[0];
+
+  for (let i = 0; i < temperatures.length; i++) {
+    let currentTemp = temperatures[i];
+
+    if (typeof currentTemp !== 'number') continue;
+    if (currentTemp > max) {
+      max = currentTemp;
+    } else if (currentTemp < min) {
+      min = currentTemp
+    }
   }
-  // 2. FIND THE BUG 
-  console.table(measurement);
-  const kelvin = measurement.value + 273;
-  return kelvin;
+  const amplitude = max - min;
+  return `max: ${max}; min: ${min}; amplitude: ${amplitude}`;
 }
-// 1. IDENTIFY 
-console.log(measureKelvin());
+const temp1 = [3,1,1];
+const temp2 = [6,2,2];
+
+calcAmplitudeNew(temp1, temp2);
