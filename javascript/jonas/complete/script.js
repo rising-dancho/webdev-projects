1e676eba9d6cbe4b31a14e964af022cdf7781f5a
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-18 08:46:08 +0800

    zxc

--- javascript/jonas/complete/script.js
+++ javascript/jonas/complete/script.js
@@ -1532,3 +1532,28 @@ TEST DATA: Test with different bill values: 275, 40, and 430
 // }
 // // 1. IDENTIFY 
 // console.log(measureKelvin());
+
+// function calcAmplitudeNew(temp1, temp2) {
+
+//     let temperatures = temp1.concat(temp2);
+  
+//     let max = temperatures[0];
+//     let min = temperatures[0];
+  
+//     for (let i = 0; i < temperatures.length; i++) {
+//       let currentTemp = temperatures[i];
+  
+//       if (typeof currentTemp !== 'number') continue;
+//       if (currentTemp > max) {
+//         max = currentTemp;
+//       } else if (currentTemp < min) {
+//         min = currentTemp
+//       }
+//     }
+//     const amplitude = max - min;
+//     return `max: ${max}; min: ${min}; amplitude: ${amplitude}`;
+//   }
+//   const temp1 = [3,1,1];
+//   const temp2 = [6,2,2];
+  
+//   calcAmplitudeNew(temp1, temp2);
