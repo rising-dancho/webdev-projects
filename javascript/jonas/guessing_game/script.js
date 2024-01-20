b03958a29a2113c1fa96bbd9dcf0747488a9a750
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-20 09:28:42 +0800

    zxc

--- javascript/jonas/guessing_game/script.js
+++ javascript/jonas/guessing_game/script.js
@@ -1,4 +1,18 @@
 'use strict';
+// PROBLEM: Create the game logic.
+
+// UNDERSTANDING THE PROBLEM 
+// Responses: 
+// 📉 Too low! 
+// 📈 Too high! 
+// 💥 You lost the game!
+// 🎉 Correct Number!
+// Score: down if wrong ; green if correct
+// #60B347 winner color
+// High Score: high score saved to a variable
+
+// BREAKING UP INTO SUB-PROBLEMS 
+// How to check the user number against the random number
 
 const number = Math.floor((Math.random() * 20)) + 1;
 // console.log(number);
@@ -9,6 +23,6 @@ document.querySelector('.check').addEventListener('click', function () {
     console.log(guess, typeof guess);
 
     if (!guess) {
-        document.querySelector('.message').textContent = '✋ No number!';
+        document.querySelector('.message').textContent = '⛔ No number!';
     }
 });
