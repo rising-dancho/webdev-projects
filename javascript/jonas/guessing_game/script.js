9244963a551c4c9d4dd4e7c20f0b5ac3e0cd4bdb
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-20 08:02:56 +0800

    zxc

--- javascript/jonas/guessing_game/script.js
+++ javascript/jonas/guessing_game/script.js
@@ -1,4 +1,13 @@
 'use strict';
 
+const clickFunction = function() {
+    const guess = Number(document.querySelector('.guess').value);
+    document.querySelector('.number').textContent = guess;
+    console.log(guess, typeof guess);
 
-console.log(document.querySelector('.message').textContent);
+    if(!guess){
+        document.querySelector('.message').textContent = '✋ No number!';
+    }
+}
+
+document.querySelector('.check').addEventListener('click', clickFunction);
