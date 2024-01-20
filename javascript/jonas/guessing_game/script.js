1fd8b9845dcdae1ff673c81cad591ae745d9273c
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-20 10:18:27 +0800

    zxc

--- javascript/jonas/guessing_game/script.js
+++ javascript/jonas/guessing_game/script.js
@@ -1,18 +1,26 @@
 'use strict';
 // PROBLEM: Create the game logic.
 
-// UNDERSTANDING THE PROBLEM 
+//  UNDERSTANDING THE PROBLEM 
 // Responses: 
-// 📉 Too low! 
 // 📈 Too high! 
+// 📉 Too low! 
 // 💥 You lost the game!
 // 🎉 Correct Number!
 // Score: down if wrong ; green if correct
 // #60B347 winner color
 // High Score: high score saved to a variable
 
-// BREAKING UP INTO SUB-PROBLEMS 
-// How to check the user number against the random number
+//  BREAKING UP INTO SUB-PROBLEMS 
+// TODO:
+// ✅ How to check the user's number against the random number?
+// ✅ how to get the random number compare with the number in the input field?
+// ❌ How and when to change the score?
+// ❌ How and when to change the high score?
+// ❌ How and when to show the winner?
+// ❌ How to implement the Responses after each number change?
+// ❌ How to reset the game after pressing "Again" button?
+
 
 const number = Math.floor((Math.random() * 20)) + 1;
 // console.log(number);
@@ -24,5 +32,14 @@ document.querySelector('.check').addEventListener('click', function () {
 
     if (!guess) {
         document.querySelector('.message').textContent = '⛔ No number!';
+    } else if (guess > number) {
+        document.querySelector('.message').textContent = '📈 Too high!';
+    } else if (guess < number) {
+        document.querySelector('.message').textContent = '📉 Too low!';
+    } else if (guess === number) {
+        document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+        document.body.style.backgroundColor = "#60B347";
     }
+
 });
