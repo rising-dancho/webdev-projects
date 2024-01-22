c75236576dedc7f72f22bb30d668aa668f969e58
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-23 00:13:28 +0800

    zxc

--- javascript/jonas/projects/modal/script.js
+++ javascript/jonas/projects/modal/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const modal = document.querySelector('.model');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+
