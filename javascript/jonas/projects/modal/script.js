c5b925ce188753d4dc82df52c2a010c7a6b77e4a
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-23 01:56:31 +0800

    zxc

--- javascript/jonas/projects/modal/script.js
+++ javascript/jonas/projects/modal/script.js
@@ -5,4 +5,7 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+// console.log(btnsOpenModal);
+for (let i = 0; i < btnsOpenModal.length; i++)
+    console.log(btnsOpenModal[i].textContent);
 
