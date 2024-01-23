db086c260e2fe586af94182a468e26d187c352e5
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-23 15:55:40 +0800

    zxc

--- javascript/jonas/projects/modal/script.js
+++ javascript/jonas/projects/modal/script.js
@@ -5,16 +5,24 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+function removeModal(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
 // console.log(btnsOpenModal);
 for (let i = 0; i < btnsOpenModal.length; i++) {
     btnsOpenModal[i].addEventListener('click', function () {
-        console.log('button clicked');
+        // console.log('button clicked');
         modal.classList.remove('hidden');
         overlay.classList.remove('hidden');
     });
 }
 
 btnCloseModal.addEventListener('click', function(){
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
+    removeModal();
+});
+
+overlay.addEventListener('click', function(){
+    removeModal();
 });
