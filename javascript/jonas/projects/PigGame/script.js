feec991825ee80eb253629445b73e729feadaab9
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-25 12:01:00 +0800

    zxc

--- javascript/jonas/projects/PigGame/script.js
+++ javascript/jonas/projects/PigGame/script.js
@@ -1,23 +1,25 @@
 'use strict';
 
 // Selecting elements
-const score0Elmnt = document.querySelector('.score');
+const score0Elmnt = document.getElementById('.score--0');
 const score1Elmnt = document.getElementById('score--1');
-const currentScoreElmnt = document.querySelector('.current-score');
+const currentScore1Elmnt = document.querySelector('#current--0');
+const currentScore2Elmnt = document.querySelector('#current--1');
 const btnRollElmnt = document.querySelector('.btn--roll');
 const btnNewGame = document.querySelector('.btn--new');
-let currentScore = 0;
-let rollDiceScore = 0;
 
-score0Elmnt.textContent = 0;
-score1Elmnt.textContent = 0;
+let player1Score = 0;
+let player2Score = 0;
+
+let currenteScore = 0;
+let diceRoll = Math.floor(Math.random() * 7);
 
 btnRollElmnt.addEventListener('click', function () {
-    currentScoreElmnt.textContent = currentScore++;
-    // console.log(currentScoreElmnt.textContent);
+    currenteScore += diceRoll;
+    currentScore1Elmnt.textContent = currenteScore;
 });
 
 
-btnNewGame.addEventListener('click', function(){
+btnNewGame.addEventListener('click', function () {
     location.reload();
 });
