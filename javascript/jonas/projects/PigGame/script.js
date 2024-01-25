1b30f16d997019dcdf4019ad7e1beea7a65eb1ea
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-25 08:05:57 +0800

    zxc

--- javascript/jonas/projects/PigGame/script.js
+++ javascript/jonas/projects/PigGame/script.js
@@ -1,8 +1,14 @@
 'use strict';
 
 // Selecting elements
-const score0Elmnt =document.querySelector('player--0');
-const score1Elmnt =document.querySelector('player--1');
+const score0Elmnt = document.querySelector('.score');
+const score1Elmnt = document.getElementById('score--1');
+const btnRollElmnt = document.querySelector('.btn--roll');
 
+
+let currentScore = 0;
+let rollDiceScore = 0;
 
 score0Elmnt.textContent = 0;
+score1Elmnt.textContent = 0;
+
