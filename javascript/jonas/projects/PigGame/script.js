73def13bfd3c9e64f81c701984637cd51b44f1dc
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-25 09:21:04 +0800

    zxc

--- javascript/jonas/projects/PigGame/script.js
+++ javascript/jonas/projects/PigGame/script.js
@@ -3,12 +3,21 @@
 // Selecting elements
 const score0Elmnt = document.querySelector('.score');
 const score1Elmnt = document.getElementById('score--1');
+const currentScoreElmnt = document.querySelector('.current-score');
 const btnRollElmnt = document.querySelector('.btn--roll');
-
-
+const btnNewGame = document.querySelector('.btn--new');
 let currentScore = 0;
 let rollDiceScore = 0;
 
 score0Elmnt.textContent = 0;
 score1Elmnt.textContent = 0;
 
+btnRollElmnt.addEventListener('click', function () {
+    currentScoreElmnt.textContent = currentScore++;
+    // console.log(currentScoreElmnt.textContent);
+});
+
+
+btnNewGame.addEventListener('click', function(){
+    location.reload();
+});
