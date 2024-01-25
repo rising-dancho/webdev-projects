a1110bf4e4f0fa0947e367d32d1aac7ed65bc644
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-26 07:58:34 +0800

    zxc

--- javascript/jonas/projects/PigGame/script.js
+++ javascript/jonas/projects/PigGame/script.js
@@ -7,59 +7,92 @@ const activePlayer1Elmnt = document.querySelector('.player--0');
 const activePlayer2Elmnt = document.querySelector('.player--1');
 const playerElmnt = document.getElementsByClassName('player--0');
 
-const currentScore1Elmnt = document.querySelector('#current--0');
-const currentScore2Elmnt = document.querySelector('#current--1');
+const currentScore1Elmnt = document.getElementById('current--0');
+const currentScore2Elmnt = document.getElementById('current--1');
 const btnRollElmnt = document.querySelector('.btn--roll');
 const btnNewGameElmnt = document.querySelector('.btn--new');
 const btnHoldElmnt = document.querySelector('.btn--hold');
 
-
-
-btnNewGameElmnt.addEventListener('click', function () {
-  location.reload();
-});
-
-let currenteScore = 0;
-let activePlayer = true;
 let player1Score = 0;
 let player2Score = 0;
+let currenteScore = 0;
+
+let activePlayer = true;
+
+function deactivatePlayer1() {
+  activePlayer1Elmnt.classList.remove('player--active');
+  activePlayer2Elmnt.classList.add('player--active');
+  console.log(activePlayer + "PLAYER 2");
+  // deactivating PLAYER 1
+  activePlayer = false;
+}
+
+function deactivatePlayer2() {
+  activePlayer2Elmnt.classList.remove('player--active');
+  activePlayer1Elmnt.classList.add('player--active');
+  console.log(activePlayer + "PLAYER 1");
+  // deactivating PLAYER 2
+  activePlayer = true;
+}
+
+// HOLD
 btnHoldElmnt.addEventListener('click', function () {
 
   score0Elmnt.textContent = currenteScore;
   currentScore1Elmnt.textContent = 0;
 
   if (activePlayer === true) {
-    // deactivating PLAYER 1
-    activePlayer1Elmnt.classList.remove('player--active');
-    activePlayer2Elmnt.classList.add('player--active');
-    // PLAYER 1
+    deactivatePlayer1();
+    // PLAYER 2
     // Adding current score to PLAYER 1 score
     player1Score = currenteScore;
     score0Elmnt.textContent = player1Score;
     currentScore1Elmnt.textContent = 0;
-    activePlayer = true;
+  } else {
+    deactivatePlayer2();
+    // PLAYER 1
+    // Adding current score to PLAYER 2 score
+    player2Score = currenteScore;
+    score1Elmnt.textContent = player2Score;
+    currentScore2Elmnt.textContent = 0;
+  }
+});
+
+// DICE ROLL
+btnRollElmnt.addEventListener('click', function () {
+  let diceRoll = Math.floor(Math.random() * 6) + 1;
+  if (activePlayer === true) {
+    // deactivating PLAYER 1
+    activePlayer1Elmnt.classList.remove('player--active');
+    activePlayer2Elmnt.classList.add('player--active');
+    console.log(activePlayer + "PLAYER 2");
+    // PLAYER 2:
+    currentScore2Elmnt.textContent = currenteScore += diceRoll;
+    player2Score = currenteScore;
+    activePlayer = false;
   } else {
     // deactivating PLAYER 2
     activePlayer2Elmnt.classList.remove('player--active');
     activePlayer1Elmnt.classList.add('player--active');
-    // PLAYER 2
-    // Adding current score to PLAYER 2 score
-    player2Score = currenteScore;
-    score0Elmnt.textContent = player2Score;
-    currentScore1Elmnt.textContent = 0;
-    activePlayer = false;
+    // PLAYER 1:
+    console.log(activePlayer + "PLAYER 1");
+    currentScore1Elmnt.textContent = currenteScore += diceRoll;
+    player1Score = currenteScore;
+    activePlayer = true;
   }
-});
-
-btnRollElmnt.addEventListener('click', function () {
-  let diceRoll = Math.floor(Math.random() * 6) + 1;
-  currentScore1Elmnt.textContent = currenteScore += diceRoll;
-  // console.log(diceRoll);
 
   switch (diceRoll) {
     case 1:
       currenteScore = 0;
-      currentScore1Elmnt.textContent = currenteScore;
+      if (activePlayer === true) {
+        // PLAYER 1:
+        currentScore1Elmnt.textContent = currenteScore;
+        activePlayer = true;
+      } else {
+        currentScore2Elmnt.textContent = currenteScore;
+        activePlayer = false;
+      }
+
       document.querySelector(".dice").setAttribute("src", './img/dice-1.png');
       break;
     case 2:
@@ -82,3 +115,7 @@ btnRollElmnt.addEventListener('click', function () {
   }
 });
 
+// NEW GAME
+btnNewGameElmnt.addEventListener('click', function () {
+  location.reload();
+});
