db030fc87cd9626b18612e9ae22195a8d8b95552
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-25 20:02:16 +0800

    zzxcxz

--- javascript/jonas/projects/PigGame/script.js
+++ javascript/jonas/projects/PigGame/script.js
@@ -5,15 +5,13 @@ const score0Elmnt = document.getElementById('score--0');
 const score1Elmnt = document.getElementById('score--1');
 const activePlayer1Elmnt = document.querySelector('.player--0');
 const activePlayer2Elmnt = document.querySelector('.player--1');
+const playerElmnt = document.getElementsByClassName('player--0');
 
 const currentScore1Elmnt = document.querySelector('#current--0');
 const currentScore2Elmnt = document.querySelector('#current--1');
 const btnRollElmnt = document.querySelector('.btn--roll');
 const btnNewGameElmnt = document.querySelector('.btn--new');
 const btnHoldElmnt = document.querySelector('.btn--hold');
-
-let player1Score = 0;
-let player2Score = 0;
 
 let currenteScore = 0;
 btnRollElmnt.addEventListener('click', function () {
@@ -51,15 +49,35 @@ btnNewGameElmnt.addEventListener('click', function () {
   location.reload();
 });
 
+
+let activePlayer = true;
+let player1Score = 0;
+let player2Score = 0;
 btnHoldElmnt.addEventListener('click', function () {
+
   score0Elmnt.textContent = currenteScore;
   currentScore1Elmnt.textContent = 0;
-  activePlayer1Elmnt.classList.remove('player--active');
-  activePlayer2Elmnt.classList.add('player--active');
-  // console.log(score0Elmnt);
+
+  if (activePlayer === true) {
+    // deactivating PLAYER 1
+    activePlayer1Elmnt.classList.remove('player--active');
+    activePlayer2Elmnt.classList.add('player--active');
+    // PLAYER 1
+    // Adding current score to PLAYER 1 score
+    player1Score = currenteScore;
+    score0Elmnt.textContent = player1Score;
+    currentScore1Elmnt.textContent = 0;
+    activePlayer = true;
+  } else {
+    // deactivating PLAYER 2
+    activePlayer2Elmnt.classList.remove('player--active');
+    activePlayer1Elmnt.classList.add('player--active');
+    // PLAYER 2
+    // Adding current score to PLAYER 2 score
+    player2Score = currenteScore;
+    score0Elmnt.textContent = player2Score;
+    currentScore1Elmnt.textContent = 0;
+    activePlayer = false;
+  }
 });
 
-// const openModal = function () {
-//   modal.classList.remove('hidden');
-//   overlay.classList.remove('hidden');
-// }
