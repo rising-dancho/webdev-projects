c5b43f54170902e1b8e5db734006d3260931c387
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-26 09:40:11 +0800

    zxc

--- javascript/jonas/projects/PigGame/script.js
+++ javascript/jonas/projects/PigGame/script.js
@@ -19,54 +19,52 @@ let player2Score = 0;
 let currentScore1 = 0;
 let currentScore2 = 0;
 
-let activePlayer = true;
-let activePlayer1 = true;
-let activePlayer2 = true;
-
+// let activePlayer = false;
+let player1Active = false;
+let player2Active = false;
+    
 function deactivatePlayer1() {
   activePlayer1Elmnt.classList.remove('player--active');
   activePlayer2Elmnt.classList.add('player--active');
-  console.log(activePlayer + "PLAYER 2");
-  // deactivating PLAYER 1
-  activePlayer = false;
+  // activating PLAYER 1
+  player1Active = true;
+  player2Active = false;
 }
 
 function deactivatePlayer2() {
   activePlayer2Elmnt.classList.remove('player--active');
   activePlayer1Elmnt.classList.add('player--active');
-  console.log(activePlayer + "PLAYER 1");
-  // deactivating PLAYER 2
-  activePlayer = true;
+  // activating PLAYER 2
+  player2Active = true;
+  player1Active = false;
 }
 
 let scoreApplied = true;
 
-// DICE ROLL
+// ROLL DICE
 btnRollElmnt.addEventListener('click', function () {
   let diceRoll = Math.floor(Math.random() * 6) + 1;
-  if (activePlayer === true) {
+  if (player1Active === true) {
     currentScore2Elmnt.textContent = currentScore2 += diceRoll;
     player2Score = currentScore2;
-    activePlayer = false;
+    
   } else {
     currentScore1Elmnt.textContent = currentScore1 += diceRoll;
     player1Score = currentScore1;
-    activePlayer = true;
   }
 
   switch (diceRoll) {
     case 1:
       // currenteScore = 0;
-      if (activePlayer === true) {
+      if (player1Active === false) {
         // PLAYER 1
+        currentScore1 = 0;
         currentScore1Elmnt.textContent = currentScore1;
-        activePlayer = true;
       } else {
         // PLAYER 2
+        currentScore2 = 0;
         currentScore2Elmnt.textContent = currentScore2;
-        activePlayer = false;
       }
-
       document.querySelector(".dice").setAttribute("src", './img/dice-1.png');
       break;
     case 2:
@@ -97,7 +95,7 @@ btnNewGameElmnt.addEventListener('click', function () {
 // HOLD
 btnHoldElmnt.addEventListener('click', function () {
 
-  if (activePlayer === true) {
+  if (player1Active === false) {
     deactivatePlayer1();
     player1Score = currentScore1;
     score1Elmnt.textContent = player1Score;
