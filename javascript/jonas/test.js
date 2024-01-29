3a802b82d7ef81c2c44330557bca7a896edad973
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-29 08:04:52 +0800

    zxc

--- javascript/jonas/test.js
+++ javascript/jonas/test.js
@@ -1,4 +1,11 @@
 'use strict';
 
+// Selecting elements
 const score0Elmnt = document.querySelector('#score--0');
 const score1Elmnt = document.querySelector('#score--1');
+const diceElmnt = document.querySelector('.dice');
+
+// Starting Conditions
+score0Elmnt.textContent = 0; 
+score1Elmnt.textContent = 0;
+diceElmnt.classList.add('hidden');
