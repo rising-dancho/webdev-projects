86acd2e2c6b720d4ab55fa4b1b0f89d3207246b6
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2024-01-31 18:42:23 +0800

    zxc

--- javascript/jonas/projects/PigGame/test.js
+++ javascript/jonas/projects/PigGame/test.js
@@ -4,6 +4,8 @@
 const score0Elmnt = document.querySelector('#score--0');
 const score1Elmnt = document.querySelector('#score--1');
 const diceElmnt = document.querySelector('.dice');
+const currentScore0Elmnt = document.getElementById('current--0');
+const currentScore1Elmnt = document.querySelector('current--1');
 const btnNewElmnt = document.querySelector('.btn--new');
 const btnRollElmnt = document.querySelector('.btn--roll');
 const btnHoldElmnt = document.querySelector('.btn--hold');
@@ -14,6 +16,8 @@ score1Elmnt.textContent = 0;
 // console.log(diceElmnt);
 diceElmnt.classList.add('hidden');
 
+let currentScore = 0;
+
 // Rolling dice functionality
 btnRollElmnt.addEventListener('click', function () {
     // Generating a random dice roll
@@ -21,10 +25,13 @@ btnRollElmnt.addEventListener('click', function () {
     // Display dice
     diceElmnt.classList.remove('hidden');
     diceElmnt.src = `./img/dice-${dice}.png`
-    // Check for rolled 1: if true, to next player
+    // Check for rolled 1: 
     if (dice !== 1){
-
+        // add dice to current score
+        currentScore += dice;
+        currentScore0Elmnt.textContent = currentScore;
     }else {
+    // if true, switch to next player
         
     }
 });
