2d45ccbfca8c697eac40a0c8dfb3ed43036ca3b4
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-02-03 10:17:51 +0800

    zxc

--- javascript/jonas/projects/PigGame/test.js
+++ javascript/jonas/projects/PigGame/test.js
@@ -1,46 +1,49 @@
-'use strict';
+"use strict";
 
 // Selecting elements
-const score0Elmnt = document.querySelector('#score--0');
-const score1Elmnt = document.querySelector('#score--1');
-const diceElmnt = document.querySelector('.dice');
-const currentScore0Elmnt = document.getElementById('current--0');
-const currentScore1Elmnt = document.querySelector('current--1');
-const btnNewElmnt = document.querySelector('.btn--new');
-const btnRollElmnt = document.querySelector('.btn--roll');
-const btnHoldElmnt = document.querySelector('.btn--hold');
+const player0ActiveElmnt = document.querySelector(".player--0");
+const player1ActiveElmnt = document.querySelector(".player--1");
+const score0Elmnt = document.querySelector("#score--0");
+const score1Elmnt = document.querySelector("#score--1");
+const diceElmnt = document.querySelector(".dice");
+const currentScore0Elmnt = document.getElementById("current--0");
+const currentScore1Elmnt = document.querySelector("current--1");
+const btnNewElmnt = document.querySelector(".btn--new");
+const btnRollElmnt = document.querySelector(".btn--roll");
+const btnHoldElmnt = document.querySelector(".btn--hold");
 
 // Starting Conditions
 score0Elmnt.textContent = 0;
 score1Elmnt.textContent = 0;
 // console.log(diceElmnt);
-diceElmnt.classList.add('hidden');
+diceElmnt.classList.add("hidden");
 
 const score = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
-
 // Rolling dice functionality
-btnRollElmnt.addEventListener('click', function () {
-    // Generating a random dice roll
-    const dice = Math.trunc(Math.random() * 6) + 1;
-    // Display dice
-    diceElmnt.classList.remove('hidden');
-    diceElmnt.src = `./img/dice-${dice}.png`
-    // Check for rolled 1: 
-    if (dice !== 1) {
-        // add dice to current score
-        currentScore += dice;
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-    } else {
-        // if true, switch to next player
-        activePlayer = activePlayer === 0 ? 1 : 0; // switch from 0 to 1;
-        currentScore = 0;
-        if (activePlayer !== 0) {
-            document.getElementById('current--0').textContent = currentScore;
-        }else {
-            document.getElementById('current--1').textContent = currentScore;
-        }   
-    }
+btnRollElmnt.addEventListener("click", function () {
+  // Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // Display dice
+  diceElmnt.classList.remove("hidden");
+  diceElmnt.src = `./img/dice-${dice}.png`;
+  // Check for rolled 1:
+  if (dice !== 1) {
+    // add dice to current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // if true, switch to next player
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    activePlayer !== 0
+      ? (document.getElementById("current--0").textContent = currentScore)
+      : (document.getElementById("current--1").textContent = currentScore);
+    // activate a player alternately
+    player0ActiveElmnt.classList.toggle("player--active");
+    player1ActiveElmnt.classList.toggle("player--active");
+  }
 });
