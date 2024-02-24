14af32f9a0a4b73a86be05e034b7f52f645f1372
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-02-25 02:25:02 +0800

    zxc

--- javascript/brad_traversy/01-variables-data-types/03-variables/script.js
+++ javascript/brad_traversy/01-variables-data-types/03-variables/script.js
@@ -10,33 +10,3 @@
 // first_name    underscore
 // FirstName     PascalCase
 // firstname     owercase
-
-let firstName = 'John';
-let lastName = 'Doe';
-let age = '30';
-
-console.log(firstName, lastName, age);
-
-console.log(age);
-
-let score = 1;
-score = 1;
-
-if (true) score = score + 1;
-console.log(score);
-
-const x = 100;
-const arr = [1, 2, 3, 4];
-
-arr.push(5);
-console.log(arr);
-
-const person = {
-    name: 'John'
-};
-
-console.log(person.name);
-person.name = 'Wick';
-console.log(person.name);
-person.email = 'johnwich@gmail.com';
-console.table(person);
