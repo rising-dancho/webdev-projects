6ffc5456f76d0bd4d3704e8654afa6957291d918
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-02-25 12:40:25 +0800

    zxcz

--- javascript/brad_traversy/01-variables-data-types/04-data-types/script.js
+++ javascript/brad_traversy/01-variables-data-types/04-data-types/script.js
@@ -1,5 +1,5 @@
 /*  ===============
-    Primitive Data Types 
+    PRIMITIVE DATA TYPES 
 ===================
 */
 
@@ -25,13 +25,24 @@ const id = Symbol('id');
 // BigInt
 const n = 23145353n;
 
-let output = n;
-
-
 /*  ===============
-    Reference Types 
+    REFERENCE TYPES : any reference type is an object
 ===================
 */
 
+// Array
+const numbers = [1, 2, 3, 4];
+
+// Object
+const person = {
+    name: "adfinem"
+}
+
+// Function
+function hello(){
+    console.log("hello");
+}
+
+let output = hello;
 
 console.log(output, typeof output);
