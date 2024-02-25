0c467ecc484bd3b99cc727cf8a49151b3143832e
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-02-25 23:58:32 +0800

    xzc

--- javascript/brad_traversy/01-variables-data-types/07-operators/script.js
+++ javascript/brad_traversy/01-variables-data-types/07-operators/script.js
@@ -1,7 +1,44 @@
 // 1. Arithmetic Operators
+let x;
 
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 10 % 8;
+
+// Concatenation
+x = 'Hello' + ' World';
+
+// Exponent
+x = 2 ** 3;
+
+// Increment
+x = 1;
+// x = x + 1;
+x++;
+
+// Decrement
+// x = x - 1;
+x--;
 
 // 2. Assignment Operators
-
+x = 10;
+x += 1;
+x -= 1;
+x *= 1;
+x /= 1;
+x %= 1;
+x **= 1;
 
 // 3. Comparison Operators
+
+x = 2 == '2'; /* loose */
+x = 2 === '2'; /* strict */
+x = 2 != '2';
+x = 2 !== '2';
+
+x = 2 > 3;
+x = 2 < 3;
+
+console.log(x);
