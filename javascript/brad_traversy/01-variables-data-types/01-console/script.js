a7580183667c6fb58bb50ac6fd787146d30d7147
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-02-25 12:31:47 +0800

    zxc

--- javascript/brad_traversy/01-variables-data-types/01-console/script.js
+++ javascript/brad_traversy/01-variables-data-types/01-console/script.js
@@ -9,16 +9,4 @@ person.name = 'John';
 person.email = 'adfinem@gmail.com';
 console.log(person.name, person.email);
 
-// String
-const firstName = 'Sarah';
-// Number
-const age = 31;
-const temp = 98.9;
-// Boolean
-const hasKids = false;
-// Null
-const aptNumber = null;
 
-let output = aptNumber;
-
-console.log(output, typeof output);
---
+++ javascript/brad_traversy/01-variables-data-types/04-data-types/script.js
@@ -0,0 +1,37 @@
+/*  ===============
+    Primitive Data Types 
+===================
+*/
+
+// String
+const firstName = 'Sarah';
+
+// Number
+const age = 31;
+const temp = 98.9;
+
+// Boolean
+const hasKids = false;
+
+// Null
+const aptNumber = null;
+
+// Undefined
+const score = undefined;
+
+// Symbol
+const id = Symbol('id');
+
+// BigInt
+const n = 23145353n;
+
+let output = n;
+
+
+/*  ===============
+    Reference Types 
+===================
+*/
+
+
+console.log(output, typeof output);
