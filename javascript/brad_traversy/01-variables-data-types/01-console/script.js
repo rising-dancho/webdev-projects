46c295293044e9de403c06a00de1d395e6fa82a3
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-02-25 12:23:26 +0800

    zxc

---
+++ javascript/brad_traversy/01-variables-data-types/01-console/script.js
@@ -0,0 +1,24 @@
+let test = 'hello';
+console.log(test);
+
+const person = {
+  name: 'Brad',
+};
+
+person.name = 'John';
+person.email = 'adfinem@gmail.com';
+console.log(person.name, person.email);
+
+// String
+const firstName = 'Sarah';
+// Number
+const age = 31;
+const temp = 98.9;
+// Boolean
+const hasKids = false;
+// Null
+const aptNumber = null;
+
+let output = aptNumber;
+
+console.log(output, typeof output);
