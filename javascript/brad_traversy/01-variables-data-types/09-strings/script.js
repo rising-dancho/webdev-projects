69b6d87b5950efd36e2505dd4a700942e2db74c2
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-02-27 22:32:47 +0800

    zxc

---
+++ javascript/brad_traversy/01-variables-data-types/09-strings/script.js
@@ -0,0 +1,35 @@
+let x;
+
+const name = 'Jose';
+const age = 31;
+
+// concatenation
+// x = 'Hello, my name is ' +  name + ' and I am ' + age + ' years old';
+
+// Template literals
+x = `Hello, my name is ${name} and I am ${age} years old`;
+
+// String Properties and Methods
+const s = 'Hello World';
+
+x = s.length;
+
+// access individual letters by position
+x = s[0];
+
+x = s.__proto__;
+
+x = s.toUpperCase();
+x = s.toLowerCase();
+
+x = s.charAt(0);
+
+x = s.indexOf('e');
+
+x = s.substring(1, 5);
+x = s.slice(-10);
+
+x = s.replace('Hell', 'Heaven');
+
+console.log(x);
+
---
+++ javascript/regex/regex.js
@@ -0,0 +1,74 @@
+function hideName(name) {
+    // split
+    const first = name.split(' ')[0];
+    console.log(first);
+  
+    // loop per character
+    for (let i = 0; i < name.length; i++) {
+      console.log(name[i]);
+    }
+  
+    for (let i = 0; i < name.length; i++) {
+      let hiddenName = '';
+  
+      hiddenName += name[i];
+  
+      return console.log(hiddenName);
+    }
+    //   iterate per character: https://www.geeksforgeeks.org/how-to-iterate-over-characters-of-a-string-in-javascript/
+    //   replace on specific index: https://bobbyhadz.com/blog/javascript-string-replace-character-at-index
+    //   replace: https://bobbyhadz.com/blog/javascript-remove-first-and-last-character-from-string
+    //   split: https://bobbyhadz.com/blog/javascript-get-first-word-of-string
+  }
+  
+  
+  
+  hideName('Anonas Mayaman');
+  
+  function replaceCharacter(string) {
+    let index = 0;
+    const replacement = "*";
+    
+    return (
+      string.slice(0, index) +
+      replacement +
+      str.slice(index + replacement.length)
+    );
+  }
+  
+  const str = 'bobby';
+  
+  const result1 = replaceCharacter(str);
+  console.log(result1); //
+  
+  let string = "foo bitlog"
+  let result = string.split('foo').join('bar');
+  
+  result = "12px (2) bar-456-foo 44".split(/\d/).join("#");
+  console.log(result);
+  
+  // REGEX: Looking for a match
+  let testString = 'my name is adfinem';
+  let regex = /name is ([a-zA-Z]+)/; //group 0 and group 1 (regex target)
+  // const match = regex.test(testString);
+  let match = regex.exec(testString);
+  
+  if(match){
+    const name = match[1];
+    console.log(name);
+  }else{
+    console.log('No match');
+  } 
+  
+  // REGEX: Looking for a match
+  testString = 'fi_lea.mp3 file-a.mp3 file1.mp3 file2.mp3 sample.txt screenshot.jpg';
+  regex = /(\w+\-?_?\w+)\.mp3/g;
+  match = regex.exec(testString);
+  
+  while(match){
+    const filename = match[1];
+    console.log(filename);
+    match = regex.exec(testString)
+  }
+  
+  // regular-expressions testing site: https://regexr.com/#native_link#
