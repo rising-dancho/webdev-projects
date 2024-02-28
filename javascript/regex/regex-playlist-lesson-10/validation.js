ffbf98eda3825af1438f6f67ca0d5a6782670d7d
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-02-28 19:27:37 +0800

    zxc

--- javascript/regex/regex-playlist-lesson-10/validation.js
+++ javascript/regex/regex-playlist-lesson-10/validation.js
@@ -1,1 +1,24 @@
 // validation script here
+const inputs = document.querySelectorAll('input');
+
+const patterns = {
+  telephone: /^\d{11}$/,
+};
+
+// validation function
+function validate(field, regex) {
+  let testForValidation = regex.test(field.value);
+  console.log(testForValidation);
+  if (testForValidation === true) {
+    field.className = 'valid';
+  } else {
+    field.className = 'invalid';
+  }
+}
+
+inputs.forEach(function (input) {
+  input.addEventListener('keyup', (e) => {
+    // console.log(e.target.attributes.name.value);
+    validate(e.target, patterns[e.target.attributes.name.value]);
+  });
+});
