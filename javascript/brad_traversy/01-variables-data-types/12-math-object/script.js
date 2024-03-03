17dc910ddd73c4c4c5e5c715d8c9bc668f2ed0ed
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-03-03 16:13:32 +0800

    zxc

---
+++ javascript/brad_traversy/01-variables-data-types/12-math-object/script.js
@@ -0,0 +1,13 @@
+let x;
+
+x = Math.sqrt(9);
+x = Math.abs(-5);
+x = Math.round(4.2);
+x = Math.ceil(4.2);
+x = Math.floor(4.9);
+x = Math.pow(2, 3);
+x = Math.min(4, 5, 3);
+x = Math.max(4, 5, 3);
+x = Math.floor(Math.random() * 10) + 1; // random num between 1-10
+
+console.log(x);
