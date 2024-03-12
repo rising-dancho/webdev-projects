018e4db0b6a859ad85d89b71afc8d08be13e7ef9
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-03-13 01:53:33 +0800

    zxc

--- javascript/data_structures/traversy-js-challenges/01-basic-challenges-1/03-calculator/calculator.js
+++ javascript/data_structures/traversy-js-challenges/01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,17 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  let result;
+  if (operator === '+') {
+    result = num1 + num2;
+  } else if (operator === '-') {
+    result = num1 - num2;
+  } else if (operator === '*') {
+    result = num1 * num2;
+  } else if (operator === '/') {
+    result = num1 / num2;
+  } else {
+    console.log('Invalid operator/input');
+  }
+  return result;
+}
 
 module.exports = calculator;
