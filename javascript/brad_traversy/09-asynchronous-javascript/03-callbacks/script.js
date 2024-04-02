867a6477c44e3d835c9c912229bffb1ae29bf58c
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-04-03 04:20:17 +0800

    zxc

--- javascript/brad_traversy/09-asynchronous-javascript/03-callbacks/script.js
+++ javascript/brad_traversy/09-asynchronous-javascript/03-callbacks/script.js
@@ -1,8 +1,35 @@
 const btn = document.querySelector('button');
 
-function callBack(e) {
-  // console.dir(e.target.classList);
-  e.target.classList.toggle('danger');
+// function callBack(e) {
+//   // console.dir(e.target.classList);
+//   e.target.classList.toggle('danger');
+// }
+
+btn.addEventListener('click', createPost);
+
+const posts = [
+  { title: 'Post One', body: 'This is post one' },
+  { title: 'Post Two', body: 'This is post two' },
+];
+
+function createPost(post, cb){
+ setTimeout(() => {
+  posts.push(post);
+  cb();
+ }, 2000);
 }
 
-btn.addEventListener('click', callBack);
+function getPosts() {
+  setTimeout(() => {
+    posts.forEach((post) => {
+      const div = document.createElement('div');
+      div.innerHTML = `
+      <strong>${post.title}</strong> - <span>${post.body}</span>
+      `;
+      document.getElementById('posts').appendChild(div);
+    });
+  }, 1000);
+}
+
+
+createPost({ title: 'Post Three', body: 'This is post three' }, getPosts)
