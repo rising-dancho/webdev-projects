639325ed3c163810b50ff939cf50abc76f753b7b
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-04-03 09:35:15 +0800

    zxc

--- javascript/brad_traversy/09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator-start/script.js
+++ javascript/brad_traversy/09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator-start/script.js
@@ -1,8 +1,44 @@
+// WRONG WAY:
+// const jokeBtn = document.getElementById('joke-btn');
+// const div = document.getElementById('joke');
+// const xhr = new XMLHttpRequest();
+
+// xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
+
+// // readyState has 5 possible values:
+// // - 0: request not established
+// // - 1: server connection established
+// // - 2: request received
+// // - 3: processing request
+// // - 4: request finished and response is ready
+// xhr.onreadystatechange = function () {
+//   // console.log(this.readyState);
+//   // console.log(this.status);
+//   //   console.log(this.responseText);
+//   if (this.readyState === 4 && this.status === 200) {
+//     // console.log(this.responseText, typeof this.responseText);
+//     const response = JSON.parse(this.responseText);
+//     // console.log(response.value);
+//     div.innerHTML = `${response.value}`;
+//   }
+// };
+
+// function generateJoke() {
+//   xhr.send();
+// }
+
+// jokeBtn.addEventListener('click', generateJoke);
+
+
+
+/* ================================================= */
+
+
+
+// CORRECT WAY: ALL xhr methods should all be inside the same function
+
 const jokeBtn = document.getElementById('joke-btn');
 const div = document.getElementById('joke');
-const xhr = new XMLHttpRequest();
-
-xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
 
 // readyState has 5 possible values:
 // - 0: request not established
@@ -10,19 +46,28 @@ xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
 // - 2: request received
 // - 3: processing request
 // - 4: request finished and response is ready
-xhr.onreadystatechange = function () {
-  // console.log(this.readyState);
-  // console.log(this.status);
-  // console.log(this.responseText);
-  if (this.readyState === 4 && this.status === 200) {
-    // console.log(this.responseText, typeof this.responseText);
-    const response = JSON.parse(this.responseText);
-    // console.log(response.value);
-    div.innerHTML = `${response.value}`;
-  }
-};
 
 function generateJoke() {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
+
+  xhr.onreadystatechange = function () {
+    // console.log(this.readyState);
+    // console.log(this.status);
+    // console.log(this.responseText);
+    if (this.readyState === 4) {
+      if (this.status === 200) {
+        // console.log(this.responseText, typeof this.responseText);
+        const response = JSON.parse(this.responseText);
+        // console.log(response.value);
+        div.innerHTML = `${response.value}`;
+      } else {
+        console.log('Whoops! Something went wrong... (not funny)');
+      }
+    }
+  };
+
   xhr.send();
 }
 
