5691470bfeb2ad8579d3e33b493c76d741be5e7b
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-04-03 09:22:14 +0800

    zxc

---
+++ javascript/brad_traversy/09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator-start/script.js
@@ -0,0 +1,29 @@
+const jokeBtn = document.getElementById('joke-btn');
+const div = document.getElementById('joke');
+const xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
+
+// readyState has 5 possible values:
+// - 0: request not established
+// - 1: server connection established
+// - 2: request received
+// - 3: processing request
+// - 4: request finished and response is ready
+xhr.onreadystatechange = function () {
+  // console.log(this.readyState);
+  // console.log(this.status);
+  // console.log(this.responseText);
+  if (this.readyState === 4 && this.status === 200) {
+    // console.log(this.responseText, typeof this.responseText);
+    const response = JSON.parse(this.responseText);
+    // console.log(response.value);
+    div.innerHTML = `${response.value}`;
+  }
+};
+
+function generateJoke() {
+  xhr.send();
+}
+
+jokeBtn.addEventListener('click', generateJoke);
