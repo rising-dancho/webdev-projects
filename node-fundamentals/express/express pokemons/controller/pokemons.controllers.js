416e2197f91d39046d743b06987168d4073ba9e6
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-05-31 20:51:13 +0800

    zxc

--- node-fundamentals/express/express pokemons/controller/pokemons.controllers.js
+++ node-fundamentals/express/express pokemons/controller/pokemons.controllers.js
@@ -21,3 +21,36 @@ export const addPokemon = (request, response) => {
     data: request.body,
   });
 };
+
+export const editPokemon = (request, response) => {
+  const pokemons = JSON.parse(fs.readFileSync('./data.json'));
+  const pokemonToEdit = pokemons.find((pokemon) => {
+    return pokemon.name.toLowerCase() === request.params.name;
+  });
+
+  if (!pokemonToEdit) {
+    response.status(404).send({ message: 'Pokemon not found.' });
+  }
+
+  for (let key in request.body) {
+    pokemonToEdit[key] = request.body[key];
+  }
+
+  fs.writeFileSync('./data.json', JSON.stringify(pokemons));
+
+  response.status(200).send({
+    message: 'Pokemon has been updated',
+    data: pokemonToEdit,
+  });
+};
+
+export const deletePokemon = (request, response) => {
+  const pokemons = JSON.parse(fs.readFileSync('./data.json'));
+  const filteredPokemons = pokemons.filter((pokemon) => {
+    return pokemon.name.toLowerCase() !== request.params.name;
+  });
+
+  fs.writeFileSync('./data.json', JSON.stringify(filteredPokemons));
+
+  response.status(204).send();
+};
--- node-fundamentals/express/express pokemons/routes/pokemons.routes.js
+++ node-fundamentals/express/express pokemons/routes/pokemons.routes.js
@@ -1,10 +1,19 @@
 import { Router } from 'express';
-import { getAllPokemons, addPokemon } from '../controller/pokemons.controllers.js';
+import {
+  getAllPokemons,
+  addPokemon,
+  editPokemon,
+  deletePokemon,
+} from '../controller/pokemons.controllers.js';
 
 const pokemonsRouter = Router();
 
 pokemonsRouter.get('/', getAllPokemons);
 
 pokemonsRouter.post('/', addPokemon);
+
+pokemonsRouter.patch('/:name', editPokemon);
+
+pokemonsRouter.delete('/:name', deletePokemon);
 
 export default pokemonsRouter;
