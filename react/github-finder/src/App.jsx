a084171f7e5a14e14574bd91c8c0304b501e29c7
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-07-25 03:51:24 +0800

    feat: section 4: 19

--- react/github-finder/src/App.jsx
+++ react/github-finder/src/App.jsx
@@ -3,11 +3,16 @@ import Navbar from './components/layouts/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios';
 import Search from './components/users/Search';
+import PropTypes from 'prop-types';
 
 class App extends Component {
   state = {
     users: [],
     loading: false,
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
   };
 
   async componentDidMount() {
@@ -18,8 +23,21 @@ class App extends Component {
       }&client_secret=${import.meta.env.VITE_CLIENT_SECRET}`
     );
     this.setState({ users: res.data, loading: false });
-    console.log(res);
+    // console.log(res);
   }
+
+  // search github users
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    // console.log(text);
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${
+        import.meta.env.VITE_CLIENT_ID
+      }&client_secret=${import.meta.env.VITE_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+    console.log(res);
+  };
 
   render() {
     return (
@@ -28,7 +46,7 @@ class App extends Component {
           <Navbar title="Github Finder" icon="fab fa-github" />
         </div>
         <div className="container">
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
