af3dcc5bfbe082be1b6f72461ad7e42878d09200
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-08-01 00:01:05 +0800

    feat: added a Home page

--- react/github-finder/src/App.jsx
+++ react/github-finder/src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layouts/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
 import Alert from './components/layouts/Alert';
 import About from './components/pages/About';
 import User from './components/users/User';
 import GithubState from './components/context/github/GithubState';
 import AlertState from './components/context/alert/AlertState';
+import Home from './components/pages/Home';
 
 function App() {
   return (
@@ -21,15 +20,7 @@ function App() {
             <div className="container">
               <Alert />
               <Routes>
-                <Route
-                  path="/"
-                  element={
-                    <>
-                      <Search />
-                      <Users />
-                    </>
-                  }
-                />
+                <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/user/:login" element={<User />} />
               </Routes>
